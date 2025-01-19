b5dc38922a46fcb8ac98dc03a8a87e6e19050261
shaden07 <106387151+shaden07@users.noreply.github.com>
2025-01-19 20:41:13 +0700

    create file test case

--- cypress/e2e/productpage/addtocart-men.cy.js
+++ cypress/e2e/productpage/addtocart-men.cy.js
@@ -2,7 +2,10 @@ describe('template spec', () => {
     it('Login Sucsess', () => {
         cy.visit('https://magento.softwaretestingboard.com')
         cy.get('#ui-id-5').click()  
-        cy.get('a').contains('Text Link').click()
+        cy.get('[class="product-image-photo"]')
+        cy.get('#btn-minicart-close').click({ force: true })
+
+
     })
     
   })
