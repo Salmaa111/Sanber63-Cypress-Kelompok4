1d50f3745c18898d98fcaaee3b4d76e7197444c7
rezaparamarta <rezaparamarta30@gmail.com>
2025-01-19 23:21:56 +0700

    Revert "Add test script positive dan negative, POM, dan config" batal This reverts commit e8a6723029b05bea50dbbfebd278deed012bd1a6.

--- cypress.config.js
+++ cypress.config.js
@@ -2,15 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    chromeWebSecurity: false,
-    viewportHeight: 660,
-    viewportWidth: 1000,
-    defaultCommandTimeout: 5000,
-    defaultBrowser: 'chrome',
   },
-  
 });
--- cypress/e2e/e2e.js
+++
@@ -1,7 +0,0 @@
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // Abaikan error terkait AddFotoramaVideoEvents
-    if (err.message.includes('AddFotoramaVideoEvents')) {
-        return false; // Mengabaikan error
-    }
-    // Biarkan error lain tetap muncul
-});
