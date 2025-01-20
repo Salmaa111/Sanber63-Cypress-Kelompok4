e20b629843cb7b677abb50be77d8a2a67cd8db5b
salmawdrt <salma.ayun29@gmail.com>
2025-01-20 21:24:25 +0700

    command edit address

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import accountInformationPage from "./pageObjectAccount/account-informationPage";
+
 Cypress.Commands.add('createAccount', (firstName, lastName, email, password) => {
     cy.visit('/customer/account/create/');
     cy.get('#firstname').type(firstName);
@@ -5,5 +7,32 @@ Cypress.Commands.add('createAccount', (firstName, lastName, email, password) => 
     cy.get('#email_address').type(email);
     cy.get('#password').type(password);
     cy.get('#password-confirmation').type(password);
+  });
+
+Cypress.Commands.add("fillAddressForm", (addressData) => {
+  cy.get("#firstname").clear().type(addressData.firstName);
+  cy.get("#lastname").clear().type(addressData.lastName);
+  cy.get("#telephone").clear().type(addressData.phone);
+  cy.get("#street_1").clear().type(addressData.street);
+  cy.get("#city").clear().type(addressData.city);
+  cy.get("#zip").clear().type(addressData.zip);
+  cy.get("#country").select(addressData.country);
+});
+
+Cypress.Commands.add("saveAddress", () => {
+  cy.get('button[title="Save Address"]').click();
+});
+  
+
+
+Cypress.Commands.add(
+  "createAccount",
+  (firstName, lastName, email, password) => {
+    cy.visit("/customer/account/create/");
+    cy.get("#firstname").type(firstName);
+    cy.get("#lastname").type(lastName);
+    cy.get("#email_address").type(email);
+    cy.get("#password").type(password);
+    cy.get("#password-confirmation").type(password);
     cy.get('button[title="Create an Account"]').click();
   });
